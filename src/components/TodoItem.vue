766e004564b841f776a3e1d64da81e0b487abc57
FaridYusifzadeh <fariddsx2015@gmail.com>
2021-04-26 23:23:47 +0400

    completed change event

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,12 +1,13 @@
 <template>
   <li> 
-      <span>
-          <input type="checkbox">
+      <span v-bind:class="{done: todo.completed}">
+          <input type="checkbox"  v-on:change="todo.completed = !todo.completed">
           <strong>{{todo.id}}</strong>
           {{todo.title}}
       </span>
-      <button>&times;</button>
+      <button class="btn-delete" v-on:click="$emit('remove-todo')">&times;</button>
   </li>
+  <!-- для того чтобы сообщить родительскому элементу что у нас чтота случилось обращаемся к функции $emit('remove-todo')    -->
 </template>
 
 <script>
@@ -16,7 +17,7 @@ export default {
           type:Object,
           required:true
       }
-  }  
+  }
 }
 </script>
 
@@ -27,5 +28,18 @@ li{
     justify-content: space-between;
     padding: .5rem 2rem;
     margin-bottom: 1rem;
+}
+.btn-delete{
+    background:red;
+    color: #fff;
+    border-radius: 50%;
+    font-weight: bold;
+    outline: none;
+}
+input{
+    margin-right: 1rem;
+}
+.done{
+    text-decoration: line-through;
 }
 </style>
